472972dabddd2402fad15dc99865cb6a6e59001e
cmpark413@gmail.com <cmpark@thinkwaresys.com>
2016-08-25 16:37:59 +0900

    modify main.cpp

--- Assignment_Intersection/main.cpp
+++ Assignment_Intersection/main.cpp
@@ -3,23 +3,18 @@
 #include <cstdlib>
 #include <set>
 
-void makeRandomData(int rangeOfRandom_begin, int rangeOfRandom_end, 
-					int sizeOfData, 
-					std::set<int>* randNumSet, char* fileName) {
+void makeRandomData(int rangeOfRandom_begin, int rangeOfRandom_end, int sizeOfData, std::set<int>* randNumSet, char* fileName) {
 
 	FILE * file = fopen(fileName, "w");
 
 	if(file != NULL) {
 
 		int randNum;
-
 		do {
 			randNum = ((rand()<<5) | rand()) % rangeOfRandom_end + rangeOfRandom_begin; // ((rand()<<5) | rand()) -> 0 ~ 1,048,575
-
 			if((*randNumSet).insert(randNum).second) {
 				fprintf(file, "%d\n", randNum);
 			}
-
 		} while((*randNumSet).size() < sizeOfData);
 
 		fclose(file);
@@ -42,16 +37,13 @@ void makeIntersectionData(std::set<int>* setA, std::set<int>* setB, char* fileNa
 		for(b_it = (*setB).begin(); b_it != (*setB).end(); ++b_it) {
 
 			a_it = (*setA).find(*b_it);
-			
 			if(a_it != (*setA).end()) {
 				fprintf(file,"%d\n", *a_it);
 			}
-
 			/*if((*setA).find(*b_it) != (*setA).end()) {
 				fprintf(file,"%d\n", *(*setA).find(*b_it));
 			}*/
 		}
-
 		fclose(file);
 	}
 }
